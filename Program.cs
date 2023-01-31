50259677ae5ef8df051e27e3c47f62e05cd4969c
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-01-31 18:38:31 +0900

    정수형식

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp
 {
-    internal class Program
+    class Program
     {
+        // 주석
+        /* 
+         * 여러 줄 
+         * 주석
+         */
         static void Main(string[] args)
         {
+            // 정수형 : byte (1 byte), short (2 byte), int (4 byte), long (8 byte), sbyte, ushort, uint, ulong
+            int hp;
+            hp = 100;
+
+            short level = 100;
+            long id;
+
+            Console.WriteLine("Hello Number ! {0}", hp);
         }
     }
 }
