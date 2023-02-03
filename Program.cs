8c7937fe8bbec72453f91d4c8faa3652d0ec1006
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-03 22:48:42 +0900

    간단한 가위바위보 게임

--- Program.cs
+++ Program.cs
@@ -6,55 +6,106 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            int hp = 0;
-            bool isDead = (hp <= 0);
+            // 0:가위 1:바위 2:보
+            Random random = new Random();
+            int aiChoice = random.Next(0, 3); // 0 ~ 2 사이의 랜덤 값
 
-            if (isDead)
-            {
-                Console.WriteLine("You are dead!");
-            }
-            else
-            {
-                Console.WriteLine("You are alive!");
-            }
+            int choice = Convert.ToInt32(Console.ReadLine());
 
-            int choice = 0; // 0 : 가위, 1 : 바위, 2: 보
-
-            if (choice == 0)
-            {
-                Console.WriteLine("가위입니다.");
-            }
-            else if (choice == 1)
-            {
-                Console.WriteLine("바위입니다.");
-            }
-            else
-            {
-                Console.WriteLine("보입니다.");
-            }
-            
-            // switch
-            choice = 4;
             switch (choice)
             {
                 case 0:
-                    Console.WriteLine("가위입니다.");
+                    Console.WriteLine("[당신] 가위!");
                     break;
                 case 1:
-                    Console.WriteLine("바위입니다.");
+                    Console.WriteLine("[당신] 바위!");
                     break;
                 case 2:
-                    Console.WriteLine("보입니다.");
-                    break;
-                default:
-                    Console.WriteLine("올바른 가위바위보를 내세요.");
+                    Console.WriteLine("[당신] 보!");
                     break;
             }
             
-            // 삼항연산자
-            int number = 25;
-            bool isPair = ((number % 2) == 0 ? true : false);
-            Console.WriteLine(isPair);
+            switch (aiChoice)
+            {
+                case 0:
+                    Console.WriteLine("[AI] 가위!");
+                    break;
+                case 1:
+                    Console.WriteLine("[AI] 바위!");
+                    break;
+                case 2:
+                    Console.WriteLine("[AI] 보!");
+                    break;
+            }
+            
+            // ver.1
+            // if (choice == 0)
+            // {
+            //     if (aiChoice == 0)
+            //     {
+            //         Console.WriteLine("[결과] 무승부!");   
+            //     }   
+            //     else if (aiChoice == 1)
+            //     {
+            //         Console.WriteLine("[결과] 패배!");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("[결과] 승리!");
+            //     }
+            // }
+            // else if (choice == 1)
+            // {
+            //     if (aiChoice == 0)
+            //     {
+            //         Console.WriteLine("[결과] 승리!");
+            //     }   
+            //     else if (aiChoice == 1)
+            //     {
+            //         Console.WriteLine("[결과] 무승부!");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("[결과] 패배!");   
+            //     }
+            // }
+            // else
+            // {
+            //     if (aiChoice == 0)
+            //     {
+            //         Console.WriteLine("[결과] 패배!");
+            //     }   
+            //     else if (aiChoice == 1)
+            //     {
+            //         Console.WriteLine("[결과] 승리!");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("[결과] 무승부!");
+            //     }
+            // }
+            
+            // ver.2
+            if (choice == aiChoice)
+            {
+                Console.WriteLine("[결과] 무승부!");
+            }
+            else if (choice == 0 && aiChoice == 2)
+            {
+                Console.WriteLine("[결과] 승리!");
+            }
+            else if (choice == 1 && aiChoice == 0)
+            {
+                Console.WriteLine("[결과] 승리!");
+            }
+            else if (choice == 2 && aiChoice == 1)
+            {
+                Console.WriteLine("[결과] 승리!");
+            }
+            else
+            {
+                Console.WriteLine("[결과] 패배!");
+            }
         }
     }
 }
