922a8c3f676e045757b43e535b03f4ea4b3f03ad
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-05 15:09:12 +0900

    break, continue

--- Program.cs
+++ Program.cs
@@ -6,10 +6,36 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            // for문
-            for (int i = 0; i < 5; i++)
+            int num = 97; // 1, 97 로만 나뉘는 숫자
+            bool isPrime = true;
+            
+            // break
+            for (int i = 2; i < num; i++)
             {
-                Console.WriteLine((i+1) + " 번째");
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break; // 여기서 if 문을 빠져나가는 것이 아니라 for, switch 같은 특정 문법 내부를 빠져나감
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine("소수입니다!");
+            }
+            else
+            {
+                Console.WriteLine("소수가 아닙니다!");
+            }
+            
+            // continue
+            for (int i = 1; i <= 100; i++)
+            {
+                if ((i % 3) != 0)
+                {
+                    continue;
+                }
+                Console.WriteLine($"3으로 나뉘는 숫자 발견 : {i}");
             }
         }
     }
