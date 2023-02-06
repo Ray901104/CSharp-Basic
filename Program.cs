eb2f58a1e0bd426a5dd27e53249f82c0dc35fc78
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-06 23:51:20 +0900

    함수

--- Program.cs
+++ Program.cs
@@ -4,39 +4,42 @@ namespace CSharp
 {
     class Program
     {
+        // 메소드 (함수)
+        // 한정자 반환타입 이름(매개변수)
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        // 덧셈함수
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void AddOne(int number)
+        {
+            number = number + 1;
+        }
+
+        static void AddOne(ref int number)
+        {
+            number = number + 1;
+        }
+
         static void Main(string[] args)
         {
-            int num = 97; // 1, 97 로만 나뉘는 숫자
-            bool isPrime = true;
-            
-            // break
-            for (int i = 2; i < num; i++)
-            {
-                if (num % i == 0)
-                {
-                    isPrime = false;
-                    break; // 여기서 if 문을 빠져나가는 것이 아니라 for, switch 같은 특정 문법 내부를 빠져나감
-                }
-            }
+            Program.HelloWorld();
+            int result = Add(4, 5);
+            Console.WriteLine("result = " + result);
 
-            if (isPrime)
-            {
-                Console.WriteLine("소수입니다!");
-            }
-            else
-            {
-                Console.WriteLine("소수가 아닙니다!");
-            }
-            
-            // continue
-            for (int i = 1; i <= 100; i++)
-            {
-                if ((i % 3) != 0)
-                {
-                    continue;
-                }
-                Console.WriteLine($"3으로 나뉘는 숫자 발견 : {i}");
-            }
+            // 복사 (짭퉁) 참조 (진퉁)
+            int a = 0;
+            Program.AddOne(a);
+            Console.WriteLine(a); // AddOne 에서 +1 한것은 복사된 값임
+
+            Program.AddOne(ref a);
+            Console.WriteLine(a); // 참조값 a 를 넣었으므로 1이 증가됨
         }
     }
 }
