c32cca32a83459df6ec122b8e2783e6469f19bb8
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-07 21:56:11 +0900

    연습문제

--- Program.cs
+++ Program.cs
@@ -4,38 +4,39 @@ namespace CSharp
 {
     class Program
     {
-        // 오버로딩
-        // 반환타입은 영향을 주지 않는다.
-        static int Add(int a, int b)
+        // 팩토리얼
+        static int Factorial(int n)
         {
-            Console.WriteLine("Add int 호출");
-            return a + b;
-        }
-        
-        // 선택적 매개변수
-        // 세 번째 매개변수로 값을 안넣으면 기본값으로 0을 사용하겠다는 의미
-        static int Add(int a, int b, int c = 0, float d = 1.0f, double e = 3.0)
-        {
-            Console.WriteLine("Add int 호출");
-            return a + b + c;
-        }
-        
-        static float Add(float a, float b)
-        {
-            Console.WriteLine("Add float 호출");
-            return a + b;
+            if (n <= 1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
         }
         
         static void Main(string[] args)
         {
-            int ret1 = Add(2, 3);
-            Console.WriteLine("ret = " + ret1);
+            // 구구단
+            for(int i = 2; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+            }
+                
+            // 별찍기
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");    
+                }
+                Console.WriteLine();
+            }
 
-            float ret2 = Add(2.0f, 3.2f);
-            Console.WriteLine("ret2 = " + ret2);
-
-            // d 자리에만 2.0f 를 넣어주고 나머지 (c, e) 는 기본값을 사용하겠다는 의미
-            Add(1, 2, d: 2.0f);
+            int ret = Factorial(5);
+            Console.WriteLine(ret);
         }
     }
 }
