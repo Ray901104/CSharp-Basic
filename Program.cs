f2233d56512ced00f2e238a95f057a43de9ab6cb
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-09 19:47:06 +0900

    TextRPG 플레이어 생성

--- Program.cs
+++ Program.cs
@@ -11,6 +11,13 @@ namespace CSharp
             Archer = 2,
             Mage = 3
         }
+        
+        // 구조체
+        struct Player
+        {
+            public int hp;
+            public int attack;
+        }
 
         static ClassType ChooseClass()
         {
@@ -37,14 +44,46 @@ namespace CSharp
             }
             return classType;
         }
+
+        static void CreatePlayer(ClassType classType, out Player player)
+        {
+            switch (classType)
+            {
+                case ClassType.Knight:
+                    player.hp = 100;
+                    player.attack = 10;
+                    break;
+                case ClassType.Archer:
+                    player.hp = 75;
+                    player.attack = 12;
+                    break;
+                case ClassType.Mage:
+                    player.hp = 50;
+                    player.attack = 15;
+                    break;
+                default:
+                    player.hp = 0;
+                    player.attack = 0;
+                    break;
+            }
+        }
         
         static void Main(string[] args)
         {
             while (true)
             {
+                // 직업 선택
                 ClassType classType = ChooseClass();
                 if (classType != ClassType.None)
                 {
+                    // 캐릭터 생성
+                    Player player;
+
+                    // 기사(100/10) 궁수(75/12) 법사(50/15)
+                    CreatePlayer(classType, out player);
+                    Console.WriteLine($"HP {player.hp} Attack {player.attack}");
+                    
+                    // 필드로 가서 몬스터와 전투
                     break;
                 }
             }
