7fe421003de1a642298bcea978a75e6ea6ae1858
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-09 17:51:26 +0900

    TextRPG 직업 고르기

--- Program.cs
+++ Program.cs
@@ -4,22 +4,50 @@ namespace CSharp
 {
     class Program
     {
-        static void Print(int value)
+        enum ClassType
         {
-            Console.WriteLine(value);
+            None = 0,
+            Knight = 1,
+            Archer = 2,
+            Mage = 3
         }
 
-        static int AddAndPrint(int a, int b)
+        static ClassType ChooseClass()
         {
-            int ret = a + b;
-            Print(ret);
-            return ret;
-        }
+            ClassType classType = ClassType.None;
+            
+            Console.WriteLine("직업을 선택하세요!");
+            Console.WriteLine("[1] 기사");
+            Console.WriteLine("[2] 궁수");
+            Console.WriteLine("[3] 법사");
 
+            string characterClass = Console.ReadLine();
+
+            switch (characterClass)
+            {
+                case "1":
+                    classType = ClassType.Knight;
+                    break;
+                case "2":
+                    classType = ClassType.Archer;
+                    break;
+                case "3":
+                    classType = ClassType.Mage;
+                    break;
+            }
+            return classType;
+        }
+        
         static void Main(string[] args)
         {
-            // 디버깅
-            int ret = Program.AddAndPrint(10, 20);
+            while (true)
+            {
+                ClassType classType = ChooseClass();
+                if (classType != ClassType.None)
+                {
+                    break;
+                }
+            }
         }
     }
 }
