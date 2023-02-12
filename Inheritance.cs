a3bfc28aed04b1af890770e4327a4ec4d0b44a1d
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-12 11:54:09 +0900

    상속성

---
+++ Inheritance.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace CSharp
+{
+    /**
+     * 객체지향프로그래밍 설계
+     * 데이터 설계 : 속성과 기능으로 분류
+     * Knight
+     * 속성 : hp, attack
+     * 기능 : Move, Attack, Die
+     */
+
+    // 상속성 : 부모클래스의 것을 자식클래스에서 사용 가능
+    // 부모 클래스
+    class PlayerClass
+    {
+        public static int count = 1;
+        public int id;
+        public int hp;
+        public int attack;
+
+        public PlayerClass()
+        {
+            Console.WriteLine("Player 생성자 호출!");
+        }
+        
+        public PlayerClass(int hp)
+        {
+            this.hp = hp;
+            Console.WriteLine("Player hp 생성자 호출!");
+        }
+        
+        // 기능
+        public void Move()
+        {
+            Console.WriteLine("Knight Move");   
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Knight Attack");
+        }
+    }
+    
+    /**
+     * Knight 객체 설계
+     * 참조
+     */
+    class Knight : PlayerClass
+    {
+        public static Knight CreateKnight()
+        {
+            Knight knight = new Knight();
+            knight.hp = 100;
+            knight.attack = 1;
+            return knight;
+        }
+        
+        // 생성자
+        public Knight() : base(100) // 부모의 hp 생성자 호출하곘다는 의미
+        {
+            Console.WriteLine("Knight 생성자 호출!");
+        }
+    }
+    
+    public class Inheritance
+    {
+        // 복사
+        struct Mage
+        {
+            public int hp;
+            public int attack;
+        }
+    
+        static void KillMage(Mage mage)
+        {
+            mage.hp = 0;
+        }
+        
+        static void KillKnight(Knight knight)
+        {
+            knight.hp = 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            knight.Move();
+            knight.Attack();
+        }
+    }
+}
--- ProgramOOP.cs
+++ ProgramOOP.cs
@@ -1,127 +1,127 @@
-﻿using System;
-
-namespace CSharp
-{
-    /**
-     * 객체지향프로그래밍 설계
-     * 데이터 설계 : 속성과 기능으로 분류
-     * Knight
-     * 속성 : hp, attack
-     * 기능 : Move, Attack, Die
-     */
-    
-    /**
-     * Knight 객체 설계
-     * 참조
-     */
-    class Knight
-    {
-        // 속성 : 인스턴스마다 다른 값을 가질 수 있다.
-        // 인스턴스에 종속적
-        public int hp;
-        public int attack;
-        
-        // static : 오로지 1개만 존재
-        public static int count = 1;
-        public int id;
-
-        // static 함수 : Knight 클래스에 종속적
-        public static void Test()
-        {
-            // static 함수 안에서는 static 변수만 접근 가능
-            // this.hp = 100;
-            count++;
-        }
-
-        public static Knight CreateKnight()
-        {
-            Knight knight = new Knight();
-            knight.hp = 100;
-            knight.attack = 1;
-            return knight;
-        }
-        
-        // 생성자
-        public Knight()
-        {
-            id = count;
-            count++;
-            
-            hp = 200;
-            attack = 20;
-            Console.WriteLine("생성자 호출!");
-        }
-        
-        public Knight(int hp) : this()
-        {
-            this.hp = hp;
-            Console.WriteLine("매개변수 생성자 호출!");
-        }
-        
-        public Knight Clone()
-        {
-            Knight knight = new Knight();
-            knight.hp = hp;
-            knight.attack = attack;
-            return knight;
-        }
-        
-        // 기능
-        public void Move()
-        {
-            Console.WriteLine("Knight Move");   
-        }
-
-        public void Attack()
-        {
-            Console.WriteLine("Knight Attack");
-        }
-    }
-    
-    class ProgramOOP
-    {
-        // 복사
-        struct Mage
-        {
-            public int hp;
-            public int attack;
-        }
-    
-        static void KillMage(Mage mage)
-        {
-            mage.hp = 0;
-        }
-        
-        static void KillKnight(Knight knight)
-        {
-            knight.hp = 0;
-        }
-        
-        static void Main(string[] args)
-        {
-            Mage mage;
-            mage.hp = 100;
-            mage.attack = 50;
-            KillMage(mage);
-            
-            Mage mage2 = mage;
-            mage2.hp = 0;
-            
-            Knight knight = new Knight(50);
-            // knight.hp = 100;
-            // knight.attack = 10;
-            // knight.Move();
-            // knight.Attack();
-            KillKnight(knight);
-
-            // 서로 분리된 객체가 됨
-            Knight knight2 = knight.Clone();
-            knight2.hp = 10;
-            
-            // static 함수 호출 : 클래스에 종속적
-            Knight knight4 = Knight.CreateKnight();
-            // Knight.Move(); --> knight.Move(); // 인스턴스에 종속적
-            Knight.Test();
-        }
-    }
-}
+﻿// using System;
+//
+// namespace CSharp
+// {
+//     /**
+//      * 객체지향프로그래밍 설계
+//      * 데이터 설계 : 속성과 기능으로 분류
+//      * Knight
+//      * 속성 : hp, attack
+//      * 기능 : Move, Attack, Die
+//      */
+//     
+//     /**
+//      * Knight 객체 설계
+//      * 참조
+//      */
+//     class Knight
+//     {
+//         // 속성 : 인스턴스마다 다른 값을 가질 수 있다.
+//         // 인스턴스에 종속적
+//         public int hp;
+//         public int attack;
+//         
+//         // static : 오로지 1개만 존재
+//         public static int count = 1;
+//         public int id;
+//
+//         // static 함수 : Knight 클래스에 종속적
+//         public static void Test()
+//         {
+//             // static 함수 안에서는 static 변수만 접근 가능
+//             // this.hp = 100;
+//             count++;
+//         }
+//
+//         public static Knight CreateKnight()
+//         {
+//             Knight knight = new Knight();
+//             knight.hp = 100;
+//             knight.attack = 1;
+//             return knight;
+//         }
+//         
+//         // 생성자
+//         public Knight()
+//         {
+//             id = count;
+//             count++;
+//             
+//             hp = 200;
+//             attack = 20;
+//             Console.WriteLine("생성자 호출!");
+//         }
+//         
+//         public Knight(int hp) : this()
+//         {
+//             this.hp = hp;
+//             Console.WriteLine("매개변수 생성자 호출!");
+//         }
+//         
+//         public Knight Clone()
+//         {
+//             Knight knight = new Knight();
+//             knight.hp = hp;
+//             knight.attack = attack;
+//             return knight;
+//         }
+//         
+//         // 기능
+//         public void Move()
+//         {
+//             Console.WriteLine("Knight Move");   
+//         }
+//
+//         public void Attack()
+//         {
+//             Console.WriteLine("Knight Attack");
+//         }
+//     }
+//     
+//     class ProgramOOP
+//     {
+//         // 복사
+//         struct Mage
+//         {
+//             public int hp;
+//             public int attack;
+//         }
+//     
+//         static void KillMage(Mage mage)
+//         {
+//             mage.hp = 0;
+//         }
+//         
+//         static void KillKnight(Knight knight)
+//         {
+//             knight.hp = 0;
+//         }
+//         
+//         static void Main(string[] args)
+//         {
+//             Mage mage;
+//             mage.hp = 100;
+//             mage.attack = 50;
+//             KillMage(mage);
+//             
+//             Mage mage2 = mage;
+//             mage2.hp = 0;
+//             
+//             Knight knight = new Knight(50);
+//             // knight.hp = 100;
+//             // knight.attack = 10;
+//             // knight.Move();
+//             // knight.Attack();
+//             KillKnight(knight);
+//
+//             // 서로 분리된 객체가 됨
+//             Knight knight2 = knight.Clone();
+//             knight2.hp = 10;
+//             
+//             // static 함수 호출 : 클래스에 종속적
+//             Knight knight3 = Knight.CreateKnight();
+//             // Knight.Move(); --> knight.Move(); // 인스턴스에 종속적
+//             Knight.Test();
+//         }
+//     }
+// }
