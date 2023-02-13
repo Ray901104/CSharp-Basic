e2f4573e6b5b49e6a7c77c7e81b191ef1025f7ae
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-13 14:35:09 +0900

    클래스 형변환

--- Capsulation.cs
+++ Capsulation.cs
@@ -1,5 +1,16 @@
 ﻿namespace CSharp
 {
+    class Player
+    {
+        protected int hp;
+        protected int attack;
+    }
+
+    class Mage : Player
+    {
+        public int mp;
+    }
+    
     // 은닉성
     class KnightCapsule
     {
@@ -25,10 +36,39 @@
     
     public class Capsulation
     {
+        static void EnterGame(Player player)
+        {
+            // 클래스 형변환 ver.1
+            bool isMage = player is Mage;
+            if (isMage)
+            {
+                Mage mage = (Mage)player;
+                mage.mp = 10;
+            }
+            
+            // 클래스 형변환 ver.2
+            // 주로 as 를 사용
+            Mage mage2 = (player as Mage);
+            if (mage2 != null)
+            {
+                mage2.mp = 10;
+            }
+        }
+        
         static void Main(string[] args)
         {
             KnightCapsule knightCapsule = new KnightCapsule();
             knightCapsule.SetHp(100);
+
+            Mage mage = new Mage();
+            EnterGame(mage);
+
+            // Mage 타입 -> Player 타입 (O)
+            // Player 타입 -> Mage 타입 (?)
+            // 자식 -> 부모 형변환은 자연스럽게 가능
+            // 부모 -> 자식 형변환은 주의 해야 함 (런타임 에러)
+            Player magePlayer = mage;
+            Mage mage2 = (Mage) magePlayer;
         }
     }
 }
