08b7c3c790daaead9b7cd4a09adf7c2575d73921
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-13 16:40:19 +0900

    다형성

--- Capsulation.cs
+++ Capsulation.cs
@@ -1,14 +1,45 @@
-﻿namespace CSharp
+﻿using System;
+
+namespace CSharp
 {
     class Player
     {
         protected int hp;
         protected int attack;
+
+        // 다형성 : virtual <-> override
+        public virtual void Move()
+        {
+            Console.WriteLine("Player 이동!");
+        }
     }
 
+    class Archer : Player
+    {
+        // override 앞에 sealed 를 붙이면 해당 클래스를 상속받는 자식클래스부터는 override 를 금지한다.
+        public override void Move()
+        {
+            base.Move(); // 부모의 Move() 호출
+            Console.WriteLine("Archer 이동!");
+        }
+    }
+
+    class SuperArcher : Player
+    {
+        public override void Move()
+        {
+            Console.WriteLine("SuperArcher 이동!");
+        }
+    }
+    
     class Mage : Player
     {
         public int mp;
+        
+        public override void Move()
+        {
+            Console.WriteLine("Mage 이동!");
+        }
     }
     
     // 은닉성
@@ -38,6 +69,7 @@
     {
         static void EnterGame(Player player)
         {
+            player.Move(); // Player.Move() --> 다형성 적용 시 객체별 함수 호출
             // 클래스 형변환 ver.1
             bool isMage = player is Mage;
             if (isMage)
@@ -52,6 +84,13 @@
             if (mage2 != null)
             {
                 mage2.mp = 10;
+                // mage2.Move();
+            }
+            
+            Archer archer = player as Archer;
+            if (archer != null)
+            {
+                // archer.Move();
             }
         }
         
@@ -61,7 +100,9 @@
             knightCapsule.SetHp(100);
 
             Mage mage = new Mage();
-            EnterGame(mage);
+            Archer archer = new Archer();
+            SuperArcher superArcher = new SuperArcher();
+            EnterGame(archer);
 
             // Mage 타입 -> Player 타입 (O)
             // Player 타입 -> Mage 타입 (?)
