9eb61409c3bdb2bf8fdff7c3a71b91dafa67e266
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-02-24 11:43:55 +0900

    Delegate

---
+++ Delegate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CSharp
+{
+    public class Delegate
+    {
+        // delegate -> 함수 자체를 인자로 넘겨주는 형식
+        // 반환 : int, 매개변수 : void
+        // OnClicked : delegate 형식의 이름
+        delegate int OnClicked();
+
+        static void ButtonPressed(OnClicked onClicked)
+        {
+            // 함수를 호출
+            onClicked();
+        }
+
+        static int TestDelegate()
+        {
+            Console.WriteLine("Hello Delegate");
+            return 0;
+        }
+        
+        static int TestDelegate2()
+        {
+            Console.WriteLine("Hello Delegate");
+            return 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            // deleage (대리자)
+            ButtonPressed(TestDelegate);
+            
+            // delegate 안의 콜백함수를 추가로 덧붙일 수 있다.
+            OnClicked onClicked = new OnClicked(TestDelegate);
+            onClicked += TestDelegate2;
+            
+            ButtonPressed(onClicked);
+        }
+    }
+}
