dd21fe4abac50636ab4be5f32f24fc2003b9b77a
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-03-01 22:28:20 +0900

    Reflection

---
+++ Reflection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+
+namespace CSharp
+{
+    public class Reflection
+    {
+        class Important : System.Attribute
+        {
+            string message;
+
+            public Important(string message)
+            {
+                this.message = message;
+            }
+        }
+        class PlayerA
+        {
+            // 컴퓨터가 런타임때 체크할 수 있는 주석
+            [Important("Very Important")]
+            public int hp;
+            protected int attack;
+            private float speed;
+            
+            void Attack(){}
+        }
+        
+        static void Main(string[] args)
+        {
+            PlayerA playerA = new PlayerA();
+
+            Type type = playerA.GetType();
+
+            FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            foreach (FieldInfo fieldInfo in fieldInfos)
+            {
+                string access = "protected";
+                if (fieldInfo.IsPublic)
+                    access = "public";
+                else if (fieldInfo.IsPrivate)
+                    access = "private";
+
+                IEnumerable customAttributes = fieldInfo.GetCustomAttributes();
+
+                Console.WriteLine($"{access} {fieldInfo.FieldType.Name} {fieldInfo.Name}");
+            }
+        }
+    }
+}
